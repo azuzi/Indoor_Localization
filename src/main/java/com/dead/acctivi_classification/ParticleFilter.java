8b58b27173eb4ee1a8431a3247d0079f2834c7d7
azuzi <azim_zain@live.com>
2019-06-20 15:54:12 +0200

    new

--- src/main/java/com/dead/acctivi_classification/ParticleFilter.java
+++ src/main/java/com/dead/acctivi_classification/ParticleFilter.java
@@ -9,6 +9,8 @@ public class ParticleFilter {
 
     int numParticles = 0;
     public int eliminateCount = 0;
+    private float xScale = 2613/48.99f;
+    private float yScale = 810/15.04f;
     Random gen = new Random();
 
     public ParticleFilter(int numParticles, PointF[] landmarks, int width, int height){
@@ -19,7 +21,7 @@ public class ParticleFilter {
         for (int i = 0; i < numParticles; i++) {
             particles[i] = new Particle(landmarks, width, height);
         }
-        // generateInitialParticles
+        // generateInitialParticles//
     }
 
 
@@ -51,6 +53,8 @@ public class ParticleFilter {
 
         particles = new_particles;//new set of particles are generated
     }
+
+
     void calNewProbability(Particle[] par){
         double totalWeight = 0;
         for (int i = 0; i < par.length; i++ ){
@@ -66,42 +70,57 @@ public class ParticleFilter {
         calNewProbability(par);
         Particle[] new_particles = new Particle[numParticles];
         int x = 0;
+        double add = 0;
         for (int i = 0; i < par.length; i++ ){
             int y = (int) Math.round(par[i].probability * Double.valueOf(eliminateCount));
-            for (int ii = 0; i < y ; ii++ ){
+            if (y == 0){ add = par[i].probability * Double.valueOf(eliminateCount); }
+            if (y > 0){
+                Double xx = Double.valueOf(y) + add;
+                y = Integer.valueOf(xx.toString());
+                for (int ii = 0; ii < y ; ii++ ) {
+                    new_particles[x + ii].set(par[i].x, par[i].y, par[i].orientation, par[i].probability);
+
+                }
+                x += y;
                 new_particles[x].set(par[i].x, par[i].y, par[i].orientation, par[i].probability);
                 x += 1;
+                add = 0;
             }
         }
+        //Particle best = getBestParticle();
         particles = new_particles;//new set of particles are generated
     }
 
     void eliminate() throws Exception {
         int count= 0;
+
         for (int i = 0; i < numParticles; i++) {
 
-            if (particles[i].x > 2.3 & particles[i].x <= 9.09 ){
-                if (particles[i].y <= 9.0 | particles[i].y >= 14.04  ){
+            if (particles[i].x > 2.3f * xScale && particles[i].x <= 9.09f * xScale){
+                if (particles[i].y <= 9.0f * yScale || particles[i].y >= 14.04f * yScale  ){
                     particles[i].set(particles[i].x, particles[i].y, particles[i].orientation,0.0);
                     count += 1;
                 }
             }
-            if (particles[i].x > 9.09 & particles[i].x <= 16.84 ){
-                if (particles[i].y <= 9.0 | particles[i].y >= 10.65  ){
+            if (particles[i].x > 9.09f * xScale && particles[i].x <= 16.84f* xScale ){
+                if (particles[i].y <= 9.0f * yScale || particles[i].y >= 10.65f * yScale ){
+                    particles[i].set(particles[i].x, particles[i].y, particles[i].orientation, 0);
+                    count += 1;
+                }
+            }
+            if (particles[i].x > 16.84f* xScale && particles[i].x <= 19.14f* xScale ){
+                if (particles[i].y <= 6.18f * yScale || particles[i].y >= 13.44f * yScale ){
                     particles[i].set(particles[i].x, particles[i].y, particles[i].orientation, 0);
                     count += 1;
                 }}
-            if (particles[i].x > 16.84 & particles[i].x <= 19.14 ){
-                if (particles[i].y <= 6.18 | particles[i].y >= 13.44  ){
+            if (particles[i].x > 19.14f * xScale& particles[i].x <= 31.43f* xScale ){
+                if (particles[i].y <= 9.0f * yScale || particles[i].y >= 10.65f * yScale  ){
                     particles[i].set(particles[i].x, particles[i].y, particles[i].orientation, 0);
                     count += 1;
-                }}
-            if (particles[i].x > 19.14 & particles[i].x <= 31.43 ){
-                if (particles[i].y <= 9.0 | particles[i].y >= 10.65  ){
-                    particles[i].set(particles[i].x, particles[i].y, particles[i].orientation, 0);
-                    count += 1;
-                }}
-            if (particles[i].x > 31.43 | particles[i].x <= 2.3 ){
+                }
+            }
+
+            if (particles[i].x > 31.43f * xScale || particles[i].x <= 2.3f * xScale ){
                     particles[i].set(particles[i].x, particles[i].y, particles[i].orientation, 0);
                 count += 1;
             }
@@ -110,7 +129,7 @@ public class ParticleFilter {
 
     }
 
-    private Particle getBestParticle() {
+    public Particle getBestParticle() {
         Particle particle = particles[0];
         for (int i = 1; i < numParticles; i++) {
             if (particles[i].probability > particle.probability) {
